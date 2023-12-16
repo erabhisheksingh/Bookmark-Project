35fad4e5c2677e1c9d96b7133208c48d8f2cb8c8
Abhishek Kumar Singh <erabhisheksingh92@gmail.com>
2023-12-16 14:54:25 +0100

    Second commit with the Flyway migration scripts

---
+++ Bookmarks-Backend/src/main/resources/db/migration/V2__insert_bookmark_table.sql
@@ -0,0 +1,3 @@
+insert into bookmarks (created_at,title,url) values (CURRENT_TIMESTAMP() ,'Google', 'www.google.com');
+insert into bookmarks (created_at,title,url) values (CURRENT_TIMESTAMP() ,'Google', 'www.google.com');
+insert into bookmarks (created_at,title,url) values (CURRENT_TIMESTAMP() ,'Google', 'www.google.com');
